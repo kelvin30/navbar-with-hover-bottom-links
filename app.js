ff7b13a912c8e8bff98b8a4333395740051a6007
kelvin30 <kelvin.rumani@gmail.com>
2023-01-29 14:09:41 +0100

    third commit

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ hamburger.addEventListener("click", () => {
     link.classList.toggle("fade");
   });
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.fontSize = "30px";
+  } else {
+    document.getElementById("navbar").style.fontSize = "220px";
+  }
+}
