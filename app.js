59ef5d0330d8af9f4834501909b0cf112648e10e
kelvin30 <kelvin.rumani@gmail.com>
2023-02-01 09:52:49 +0100

    other commit

--- app.js
+++ app.js
@@ -12,13 +12,21 @@ hamburger.addEventListener("click", () => {
 });
 
 
-/* JavaScript function for the navbar when scrolled down*/
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("navbar").style.fontSize = "30px";
-  } else {
-    document.getElementById("navbar").style.fontSize = "220px";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("navbar").style.fontSize = "30px";
+//   } else {
+//     document.getElementById("navbar").style.fontSize = "220px";
+//   }
+// }
+
+/* JavaScript function for the navbar when scrolled down*/
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function(){
+  let value = window.scrollY;
+
+  navbar.style.top = 30 + value * -.1 + '%';
+})
